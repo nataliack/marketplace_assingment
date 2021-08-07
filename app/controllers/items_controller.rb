edb066100f52bae9e635a9cc92e2613b7edbded0
NataliaCK <natalia.chamom@gmail.com>
2021-08-07 17:06:14 +1000

    finished making the part where users are able to upload items to sell

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -64,6 +64,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :description, :price, :availability, :category, :user_id)
+      params.require(:item).permit(:name, :description, :price, :availability, :category, :user_id, :picture)
     end
 end
